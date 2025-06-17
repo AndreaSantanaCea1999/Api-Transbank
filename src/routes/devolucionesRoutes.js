adbed6339dd4b634ce468e8c8fd8140c5bc047f3
AndreaSantanaCea1999 <an.santanac@duocuc.cl>
2025-06-16 23:22:29 -0400

    comenzar dnvo

--- src/routes/devolucionesRoutes.js
+++
@@ -1,7 +0,0 @@
-const devolucionesController = require('../controllers/devolucionesController');
-
-const devolucionesRouter = express.Router();
-
-devolucionesRouter.post('/', devolucionesController.solicitarDevolucion);
-
-module.exports = devolucionesRouter;
